6d58ad7fc3ee64dc958af08b0faef5d809acd0c9
Erty Seidohl <ertysdl+github@gmail.com>
2020-07-26 03:23:56 -0500

    WIP Working

--- script.js
+++ script.js
@@ -1,71 +1,112 @@
-const DAY_WIDTH = 30
-const DAY_HEIGHT = 30
+const DAY_WIDTH = 100
+const DAY_HEIGHT = 100
 const DAY_PADDING_X = 5
 const DAY_PADDING_Y = 5
+
+// This may be a bit incorrect during leap years
+// But all my data is in 2019 so /shrug
+const MIN_PER_DAY = 1440
 
 moment.tz.add("America/New_York|EST EDT EWT EPT|50 40 40 40|01010101010101010101010101010101010101010101010102301010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-261t0 1nX0 11B0 1nX0 11B0 1qL0 1a10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 RB0 8x40 iv0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1qN0 WL0 1qN0 11z0 1o10 11z0 1o10 11z0 1o10 11z0 1o10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1cN0 1cL0 1cN0 1cL0 s10 1Vz0 LB0 1BX0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 1cN0 1fz0 1a10 1fz0 1cN0 1cL0 1cN0 1cL0 1cN0 1cL0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 14p0 1lb0 14p0 1lb0 14p0 1nX0 11B0 1nX0 11B0 1nX0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Rd0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0 Op0 1zb0|21e6")
 
 async function getData() {
     const csvData = await d3.csv("./Reddit Log 2019 - All.csv")
-    const datemap = csvData.reduce((acc, curr) => {
-        if (!acc.get(curr.date)) {
-            acc.set(curr.date, [])
-        }
-        const start = moment.tz(curr.date + "T" + curr.start, "YYYY-MM-DDTHH:mm", "America/New_York")
-        const stop =  moment.tz(curr.date + "T" + curr.stop, "YYYY-MM-DDTHH:mm", "America/New_York")
-        acc.get(curr.date).push({start, stop})
-        return acc
-    }, new Map())
 
-    const datearr = []
-    datemap.forEach((v, k) => {
-        datearr.push({date: k, times: v})
+    return csvData.map(d => {
+        const date = moment.tz(d.date + "T00:00", "YYYY-MM-DDTHH:mm", "America/New_York")
+        const start = moment.tz(d.date + "T" + d.start, "YYYY-MM-DDTHH:mm", "America/New_York")
+        const stop =  moment.tz(d.date + "T" + d.stop, "YYYY-MM-DDTHH:mm", "America/New_York")
+        return {date, start, stop}
     })
-    return datearr
+}
+
+function indexToX(i) {
+    return (i % 7) * (DAY_WIDTH + DAY_PADDING_X)
+}
+
+function indexToY(i) {
+    return ((i / 7) | 0) * (DAY_HEIGHT + DAY_PADDING_Y)
+}
+
+function getSlabHeight(d) {
+    const startMins = ((d.start.hour() * 60) + d.start.minute())
+    const endMins = ((d.stop.hour() * 60) + d.stop.minute())
+
+    if (endMins < startMins) {
+        console.log(d.date.format("YYYY-MM-DD"))
+    }
+
+    return ((endMins - startMins) / MIN_PER_DAY) * DAY_HEIGHT
+}
+
+function getSlabX(d, firstDay) {
+    return (d.date.diff(firstDay, "days") % 7) * (DAY_WIDTH + DAY_PADDING_X)
+}
+
+function getSlabY(d, firstDay) {
+    const dayOffset = ((d.date.diff(firstDay, "days") / 7) | 0) * (DAY_HEIGHT + DAY_PADDING_Y)
+    const timePercentage = ((d.start.hour() * 60) + d.start.minute()) / MIN_PER_DAY
+    return dayOffset + (timePercentage * DAY_HEIGHT)
+}
+
+function onMouseOver(day, tooltip) {
+    const momentDate = moment.tz(day, "YYYY-MM-DD", "America/New_York")
+    tooltip.transition()
+        .duration(100)
+        .style("opacity", .9);
+    tooltip.html(momentDate.format("ddd, MMM Do"))
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+}
+
+function onMouseOut(tooltip) {
+    tooltip.transition()
+        .duration(100)
+        .style("opacity", 0);
 }
 
 function drawSVG(data) {
     FIRST_DAY = moment.tz(data[0].date, "YYYY-MM-DD", "America/New_York").startOf("week")
     LAST_DAY = moment.tz(data[data.length - 1].date, "YYYY-MM-DD", "America/New_York").endOf("week")
 
-    all_weeks = []
+    all_days = []
     curr_day = FIRST_DAY.clone()
     while (curr_day.isBefore(LAST_DAY)) {
-        current_week = []
-        for(let i = 0; i < 7; i++) {
-            current_week.push(curr_day.format("YYYY-MM-DD"))
-            curr_day.add(1, "day")
-        }
-        all_weeks.push(current_week)
+        all_days.push(curr_day.format("YYYY-MM-DD"))
+        curr_day.add(1, "day")
     }
 
     const svg = d3.select("#container")
         .append("svg")
         .attr("width", DAY_WIDTH * 9)
-        .attr("height", DAY_HEIGHT * all_weeks.length)
+        .attr("height", (DAY_HEIGHT + DAY_PADDING_Y) * (all_days.length / 7))
         .attr("font-family", "sans-serif")
         .attr("font-size", 10);
 
-    const weeks = svg.selectAll("g")
-        .data(all_weeks)
-        .join("g")
-            .attr("class", "week")
-            .attr("transform", (_, i) => "translate(" + DAY_WIDTH + "," + ((DAY_HEIGHT + DAY_PADDING_Y) * i) + ")")
-            .selectAll("rect.day")
-                .data(d => d)
-                .join('rect')
-                    .attr("class", "day")
-                    .attr("fill", "#eee")
-                    .attr("x", (_, i) => (DAY_WIDTH + DAY_PADDING_X) * i)
-                    .attr("y", 0)
-                    .attr("width", DAY_WIDTH)
-                    .attr("height", DAY_HEIGHT)
-                    .selectAll("rect.time")
-                        .data(d => data[d])
-                        .join("rect")
-                            .attr("class", "time")
-                            .attr("fill", "green")
-                            .attr("x", )
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    const dayBoxes = svg.selectAll("rect.day")
+        .data(all_days)
+            .join("rect")
+            .attr("class", "day")
+            .attr("width", DAY_WIDTH)
+            .attr("height", DAY_HEIGHT)
+            .attr("x", (_, i) => indexToX(i))
+            .attr("y", (_, i) => indexToY(i))
+            .attr("fill", "#eee")
+            .on("mouseover", d => onMouseOver(d, tooltip))
+            .on("mouseout", _ => onMouseOut(tooltip))
+
+    const timeSlabs = svg.selectAll("rect.time")
+        .data(data)
+            .join("rect")
+            .attr("class", "time")
+            .attr("width", DAY_WIDTH)
+            .attr("height", d => getSlabHeight(d))
+            .attr("x", d => getSlabX(d, FIRST_DAY))
+            .attr("y", d => getSlabY(d, FIRST_DAY))
 
 
 }
